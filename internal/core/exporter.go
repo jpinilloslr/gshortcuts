a1c9d17bf17f53f0229515e5419c9dbd1a9c7b23
joaquin <jpinilloslr@gmail.com>
2025-05-24 15:16:50 +0200

    update exporter to include built in shortcuts

--- internal/core/exporter.go
+++ internal/core/exporter.go
@@ -18,30 +18,48 @@ func NewExporter() *Exporter {
 }
 
 func (i *Exporter) Export(fileName string, verbose bool) error {
-	shortcuts, err := i.manager.GetCustomShortcuts()
+	builtInShortcuts, err := i.manager.GetBuiltInShortcuts(true)
+	if err != nil {
+		return err
+	}
+
+	customShortcuts, err := i.manager.GetCustomShortcuts()
 	if err != nil {
 		return err
 	}
 
 	data := Shortcuts{
-		Custom: shortcuts,
+		BuiltIn: builtInShortcuts,
+		Custom:  customShortcuts,
 	}
 
 	if err := i.codec.Encode(&data, fileName); err != nil {
 		return err
 	}
 
+	totalCount := len(customShortcuts)
+
+	for schema, shortcuts := range builtInShortcuts {
+		totalCount += len(shortcuts)
+		if verbose {
+			fmt.Printf("Exported %d shortcuts in \"%s\"\n", len(shortcuts), schema)
+			for _, shortcut := range shortcuts {
+				fmt.Printf("\t%s: %+v\n", shortcut.Key, shortcut.Bindings)
+			}
+			fmt.Println()
+		}
+	}
+
 	if verbose {
-		for _, shortcut := range shortcuts {
-			fmt.Printf("Exported shortcut: %s\n", shortcut.Name)
-			fmt.Printf("\tCommand: %s\n", shortcut.Command)
-			fmt.Printf("\tBinding: %s\n", shortcut.Binding)
+		fmt.Printf("Exported %d custom shortcuts\n", len(customShortcuts))
+		for _, shortcut := range customShortcuts {
+			fmt.Printf("\t%s: %s\n", shortcut.Id, shortcut.Binding)
 		}
 		fmt.Println()
 	}
 
-	fmt.Printf("%s Exported %d shortcuts to %s\n",
-		color.GreenString("✔"), len(shortcuts), fileName)
+	fmt.Printf("%s Exported %d total shortcuts to %s\n",
+		color.GreenString("✔"), totalCount, fileName)
 
 	return nil
 }
